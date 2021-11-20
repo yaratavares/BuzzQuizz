45b7f4cdd7ca3b1ef6290481a7b4bb6a4f1e6e74
carolyza <caroline.y.ldm@gmail.com>
2021-11-19 23:18:10 -0300

    js pagina 2

--- main.js
+++ main.js
@@ -1,2 +1,54 @@
-const quizzes = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
-quizzes.then();
+function searchQuizz(idQuizz) {
+    let quizzes = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/' + idQuizz);
+    quizzes.then(openQuizz);
+}
+
+function openQuizz(answers) {
+    let elementTitle = document.querySelector(".layer h2");
+
+    let elementOptions = document.querySelector(".answers");
+    let elementBackground = document.querySelector(".title");
+    let elementQuestions = document.querySelector(".allQuestions");
+
+
+
+
+    elementBackground.style.backgroundImage = `url(${answers.data.image})`;
+    elementTitle.innerHTML = answers.data.title;
+    console.log(answers.data.questions);
+
+
+    for (let i = 0; i < answers.data.questions.length; i++) {
+        elementQuestions.innerHTML += `<div class="quizzBoxes"> <div class="question">
+        <h3>${answers.data.questions[i].title}</h3>
+        </div><div class ="answers"></div></div>`;
+    }
+
+    let elementBoxes = document.querySelectorAll(".answers");
+
+    for (let i = 0; i < elementBoxes.length; i++) {
+        let arrayAnswers = answers.data.questions[i].answers
+        arrayAnswers.sort(shuffle);
+
+        for (let x = 0; x < arrayAnswers.length; x++) {
+            elementBoxes[i].innerHTML += `
+    <div class="answer${x+1} option">
+        <img src=${arrayAnswers[x].image}>
+        <p>${arrayAnswers[x].text}</p>
+    </div>`
+        }
+    }
+
+    let elementColor = document.querySelectorAll(".question");
+
+    for (let i = 0; i < answers.data.questions.length; i++) {
+        let color = answers.data.questions[i].color;
+        elementColor[i].style.background = color;
+    }
+
+
+}
+
+function shuffle() {
+    return Math.random() - 0.5;
+}
