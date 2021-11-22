021cd96d558c9e36c1406e06c25d66dd5da32209
Yara Tavares <yaracriistavares@gmail.com>
2021-11-22 00:48:41 -0300

    criterio perguntas

--- main.js
+++ main.js
@@ -120,7 +120,13 @@ function selectAnswer(option) {
 
 
 //criacao do quiz
+// let quiz = {title: "Título do quizz", image: "https://http.cat/411.jpg",};
 let quiz = {};
+let qtdP;
+let qtdN;
+let questions = [];
+let conter = 1;
+
 function createQuiz (){
     const elementTelaInicio = document.getElementById("telaInicio");
     const elementCreateQuiz = document.querySelector("#criarQuizzes .section1");
@@ -128,10 +134,136 @@ function createQuiz (){
     elementCreateQuiz.classList.remove("hidden");
 }
 
-function createSection1(){
-    const title = document.getElementById('title');
-    const image = document.getElementById('imageQuiz')
+function saveSection1 (){
+    const title = (document.getElementById('title')).value;
+    const image = (document.getElementById('imageQuiz')).value;
+    qtdP = (document.getElementById('qtdP')).value;
+    qtdN = (document.getElementById('qtdN')).value;
+    
+    const imageURL = checkURL(image);
+    
+    if( title.length < 20 || title.length > 65 || qtdP < 3 || qtdN < 2 || !imageURL){
+        alert('Preenchar os dados corretamente');
+      } else {
+          quiz = {title  , image};
+          console.log(quiz);
+      }
 
-    quiz = {title: title.value, image: image.value };
-    console.log(quiz);
+    const elementSection1 = document.querySelector("#criarQuizzes .section1");
+    const elementSection2 = document.querySelector("#criarQuizzes .section2");
+    elementSection1.classList.add("hidden");
+    elementSection2.classList.remove("hidden");
+
+    displaySection2()
+}
+
+function displaySection2 (){
+    const secao = document.querySelector("#criarQuizzes .section2 .containerQuestions");
+    for (let i=1; i<= qtdP; i++){
+        secao.innerHTML +=
+        `<div class="outrasPerguntasNiveis">
+            <p class="question${i}">Pergunta ${i}</p>
+            <ion-icon name="create-outline" onclick="openQuestion(this)"></ion-icon>
+        </div>`
+    }
+}
+
+function displayQuestions (secao) {
+        secao.innerHTML += 
+            `<ion-icon name="create-outline" onclick="openQuestion(this)"></ion-icon>
+            <input type="text" id="question${conter}" placeholder="Texto da pergunta" />
+            <input type="text" id="color${conter}" placeholder="Cor de fundo da pergunta" />
+            <p>Resposta correta</p>
+            <input type="text" id="answerCorrect${conter}" placeholder="Resposta correta" />
+            <input type="text" id="urlCorrect${conter}" placeholder="URL da imagem" />
+            <p>Respostas incorretas</p>
+            <input type="text" id="answerWrong1_${conter}" placeholder="Resposta incorreta 1" />
+            <input type="text" id="urlWrong1${conter}" placeholder="URL da imagem 1" />
+            <br>
+            <input type="text" id="answerWrong2_${conter}" placeholder="Resposta incorreta 2" />
+            <input type="text" id="urlWrong2${conter}" placeholder="URL da imagem 2" />
+            <br>
+            <input type="text" id="answerWrong3_${conter}" placeholder="Resposta incorreta 3" />
+            <input type="text" id="urlWrong3${conter}" placeholder="URL da imagem 3" />`;
+}
+
+function openQuestion (icon){
+    const div = icon.parentNode;
+    if (conter === 1){
+        div.classList.remove("outrasPerguntasNiveis");
+        div.classList.add('containerInput');
+        displayQuestions(div);
+    } else {
+        let valid = saveSection2();
+        if (valid){
+            div.classList.remove("outrasPerguntasNiveis");
+            div.classList.add('containerInput');
+            displayQuestions(div);
+        } else {
+            return;
+        }
+    }
+    conter++;
+}
+
+ function saveSection2 (){
+    let i = conter - 1;
+    let question = (document.getElementById(`${'question' + i}`)).value;
+    let color = (document.getElementById(`${'color' + i}`)).value;
+    let answerCorrect = (document.getElementById(`${'answerCorrect' + i}`)).value;
+    let urlCorrect = (document.getElementById(`${'urlCorrect' + i}`)).value;
+    let answerWrong1 = (document.getElementById(`${'answerWrong1_' + i}`)).value;
+    let urlWrong1 = (document.getElementById(`${'urlWrong1' + i}`)).value;
+    let answerWrong2 = (document.getElementById(`${'answerWrong2_' + i}`)).value;
+    let urlWrong2 = (document.getElementById(`${'urlWrong2' + i}`)).value;
+    let answerWrong3 = (document.getElementById(`${'answerWrong3_' + i}`)).value;
+    let urlWrong3 = (document.getElementById(`${'urlWrong3' + i}`)).value;
+    let answer = [];
+
+    let valid1 = checkURL(urlCorrect);  
+    let valid2 = checkURL(urlWrong1);
+    console.log(urlWrong1);
+    if (question.length < 20 || !(color.includes('#')) || !(color.length === 7) || answerCorrect === null || answerWrong1 === null || !valid1 || !valid2) {
+        alert('Preenchar os dados corretamente: pergunta');
+    } else {
+        questions.push([{tittle: question, color}]);
+        answer.push ([{text: answerCorrect, image: urlCorrect, isCorrectAnswer: true}]);
+        answer.push ([{text: answerWrong1, image: urlWrong1, isCorrectAnswer: false}]);
+        
+        if (answerWrong2 !== null && checkURL(urlWrong2)){
+            answer.push ([{text: answerWrong2, image: urlWrong2, isCorrectAnswer: false}]);
+            if (answerWrong3 !== null && checkURL(urlWrong3)){
+                answer.push ([{text: answerWrong3, image: urlWrong3, isCorrectAnswer: false}]);
+            }
+        }  
+            questions.answer = answer;
+            quiz.questions = questions;
+            console.log(quiz)
+            return true;
+        
+    }
+ 
+    if (i === qtdP){
+        displaySection3();
+    }
+}
+
+
+function displaySection3 (){
+    console.log('cheguei na tela 3')
+}
+
+
+function checkURL(url) {
+    if(url.includes('.jpeg')){
+        return true;
+    } else if (url.includes('.jpg')){
+        return true;
+    } else if (url.includes('gif')){
+        return true;
+    } else if (url.includes('png')){
+        return true
+    } else {
+        return false;
+    }
 }
